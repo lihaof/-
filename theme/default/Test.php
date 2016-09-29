09e1254436b1e27f8f468fe663d6882b3a5e1ab9
RuphiLau <rf126@qq.com>
2016-09-29 20:35:51 +0800

    模板样例

---
+++ theme/default/Test.php
@@ -0,0 +1,39 @@
+<?php !defined("FCPATH") && exit("Access Denied!"); ?>
+
+<!--{execute}-->
+header("Content-type: text/plain");
+<!--{execute}-->
+
+输出SiteUrl：
+
+{:site_url('Home')}
+
+输出BaseUrl：
+
+{:base_url('js/jquery.js')}
+
+输出，something不一定为变量，也可以为表达式：
+{:$something}
+
+判断：
+<!--{if $condition}-->
+	1
+<!--{elseif $cond2}-->
+	2
+<!--{else}-->
+	3
+<!--{/if}-->
+
+foreach：
+形式1：
+<!--{foreach $arr $val}-->
+<!--{/foreach}-->
+形式2：
+<!--{foreach $arr $key $val}-->
+<!--{/foreach}-->
+
+执行PHP代码：
+<!--{execute}-->
+echo "Hello World";
+<!--{/execute}-->
+
