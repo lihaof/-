0576219c4e3a08fea781ca35c35720082c46a17c
RuphiLau <rf126@qq.com>
2016-09-29 20:10:47 +0800

    模板测试文件

---
+++ application/controllers/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test extends CI_Controller {
+	public function __construct() {
+		$this->load->model("initor");
+	}
+
+	public function index() {
+		// 模板文件放在 theme/default 目录里，如 Home.php 文件采用
+		// $this->ui->load("Home") 调用
+		$this->ui->load("xxx");
+	}
+}
