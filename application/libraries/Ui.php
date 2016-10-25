030781e22f6a4898f2f44dbd38c797d8e93a3564
RuphiLau <rf126@qq.com>
2016-10-25 21:26:03 +0800

    加入模板写法说明

--- application/libraries/Ui.php
+++ application/libraries/Ui.php
@@ -1,5 +1,32 @@
 <?php
 
+/**
+ *	简易模板引擎V1.0
+ *	@Author: RuphiLau
+ *
+ *	使用说明：
+ *		Ui.php是一个简易的模板引擎，它作用于MVC模式中的View层，是在View的基础上
+ *	搭设的一层解析器，目的是为了解决Codeigniter中用原生PHP写模板的繁琐和Parser库
+ *	的效率低问题。
+ *		Ui.php是一个解析器，它会将自定义的模板语句编译为PHP可识别的语句，并自动写入
+ *	application/views文件夹，采用Ui.php后，无需再使用$this->load->view了，因为
+ *	$this->load->view会被自动调用
+ *
+ *	使用Ui.php很简单，只需三个步骤：
+ *	1、采用Ui.php自定义的模板语句语法编写模板文件
+ *	2、模板文件放入theme/default文件夹里
+ *	3、原来应该用$this->load->view的地方，改为用$this->ui->load，其他地方不变
+ *
+ *	Ui.php模板语句语法：
+ *	1、采用{xxx}{/xxx}形式定界，如{if}{/if}
+ *	2、可以在 { 前面加入 <!-- 或者 /*，在 } 后面加入 !--> 或者 ※/（※用*替换）修饰
+ *	3、原来的 <?php echo $param; ?>改用 {:$param}，即{:要echo的表达式或者变量}
+ *	4、原来的 <?php foreach($arr as $key => $val): ?>
+ *	   改为 {foreach $arr $key $val}，<?php endforeach; ?> 改为 {/foreach}
+ *	5、原来的 <?php if($xxx): ?> 改为 {if $xxx}，<?php endif; ?> 改为 {/if}
+ *	6、原来的 <?php …… ?> 改为 {execute}……{/execute}
+ */
+
 class Ui {
 	private $CI;
 	private $themeName;
