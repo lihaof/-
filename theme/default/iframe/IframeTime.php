2a5bc222c604be81245b68c41e530cd2b48c3c0c
weafung <weafung@163.com>
2016-11-13 21:19:08 +0800

    修改添加开放时段时选择框错位问题

--- theme/default/iframe/IframeTime.php
+++ theme/default/iframe/IframeTime.php
@@ -26,7 +26,7 @@
         <!--{execute}-->
             $query = $this->db->get("open_time");
             $list = $query->result_array();
-            $num_rows  = $query->num_rows();
+            $tabMaxId = $this->db->select_max('time_id')->get("open_time")->first_row('array')['time_id'];
         <!--{/execute}-->
         <!--{foreach $list $key $val}-->
         <tr id="tab{:$val['time_id']}">
@@ -39,7 +39,7 @@
                 <td id="state{:$val['time_id']}"><!--{if $val['status']=='1'}-->启用<!--{elseif $val['status']=='2'}-->停用<!--{/if}--></td>
                 <td style="padding: 0"><button id="edit{:$val['time_id']}" type="button">修 改</button></td>
                 <td style="padding: 0"><button id="stop{:$val['time_id']}" type="button">状态切换</button></td>
-                <td style="padding: 0"><button id="delete{:$val['time_id']}" type="button">删 除</button></td>      
+                <td style="padding: 0"><button id="delete{:$val['time_id']}" type="button">删 除</button></td>  
             </form>
         </tr>
         <!--{/foreach}-->
@@ -171,11 +171,10 @@
         });
     });
 
-    var id = {:$num_rows}+1;
+    var id = {:$tabMaxId}+1;
     $('#add').click(function (e) {
         e.preventDefault();
-        var length = ++id;
-        length += 2;
+        var length = id++;
         //添加时段
         var tr = $("<tr id="+ 'tab' + length +">" +
             "<form action='' method=''>" +
