84c263f3ab96f006908a7a5ebbb54710f1354df4
DESKTOP-9L427PL\cg <1843292793@qq.com>
2016-11-15 02:01:06 +0800

    删除team后台错误调用initor

--- application/controllers/Admin/Team.php
+++ application/controllers/Admin/Team.php
@@ -9,8 +9,9 @@
 class Team extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
-		$this->load->model("initor");
 		$this->load->database();
+		$this->load->library("ui");
+		$this->load->helper("url");
         $this->load->helper("common_helper");
 	}
 	public function index() {
